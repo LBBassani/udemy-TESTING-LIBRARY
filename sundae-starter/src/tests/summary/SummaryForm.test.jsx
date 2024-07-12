94d8e5483af4e2e7e5f399ec4cc775e1afc2a210
LBBassani <lorena.b.bassani@gmail.com>
2024-07-12 17:58:31 -0300

    test uses userEvent now :technologist:

--- sundae-starter/src/tests/summary/SummaryForm.test.jsx
+++ sundae-starter/src/tests/summary/SummaryForm.test.jsx
@@ -1,14 +1,16 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event';
 import { describe, expect, test } from 'vitest'
 import SummaryForm from '../../pages/summary/SummaryForm';
 
 
 describe('Testing summary form', () => {
+    const user = userEvent.setup();
     
     /* testa se ao inicio o botão está desabilitado e a checkbox está desmarcada
-    * testa se ao concordar com os termos o botão de envio do formulário é habilitado 
-    */
-   test('Agreement checkbox enables submit button', () =>{
+     * testa se ao concordar com os termos o botão de envio do formulário é habilitado 
+     */
+    test('Agreement checkbox enables submit button', async () =>{
         render(<SummaryForm />);
         const consentCheckbox = screen.getByRole('checkbox', {name: /terms and conditions/i});
         const submitButton = screen.getByRole('button', {name: /confirm order/i});
@@ -16,12 +18,12 @@ describe('Testing summary form', () => {
         expect(consentCheckbox).not.toBeChecked();
         expect(submitButton).toBeDisabled();
 
-        fireEvent.click(consentCheckbox);
+        await user.click(consentCheckbox);
 
         expect(consentCheckbox).toBeChecked();
         expect(submitButton).toBeEnabled();
 
-        fireEvent.click(consentCheckbox);
+        await user.click(consentCheckbox);
     
         expect(consentCheckbox).not.toBeChecked();
         expect(submitButton).toBeDisabled();
