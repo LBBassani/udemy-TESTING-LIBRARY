a425dcb3351157ae96d7628a72f1bd6bab01643e
LBBassani <lorena.b.bassani@gmail.com>
2024-07-15 14:57:20 -0300

    make the scoops render test :alembic:

---
+++ sundae-starter/src/pages/entry/Options.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ScoopOption from "./ScoopOption";
+import { Row } from "react-bootstrap";
+
+export default function Options({optionType}){
+
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        axios.get(`http://localhost:3030/${optionType}`)
+        .then(response => setItems(response.data))
+        .catch((error) => {
+            console.log(error);
+        })
+    }, [optionType]);
+
+    const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+
+    return <Row>{items.map( item => <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath}/>)}</Row>
+}
